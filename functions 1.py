8c068864b9e7b85bcf377b6f092cb8365dc3dd40
veerabhadragalate01-max <veerabhadragalate01@gmail.com>
2025-10-10 14:58:44 +0530

    functions

---
+++ functions 1.py
@@ -0,0 +1,7 @@
+word="wonderful"
+def find_length(a):
+    print(f"The given word{a},The length of given word  {len(a)}")
+def reverse(word):
+    print(word[::-1])
+reverse(word)
+# find_length(word)
